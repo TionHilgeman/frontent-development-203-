a70c2fc8c3044f38b3c6db608b1110ac216fba11
TionHilgeman <tionhilgeman@gmail.com>
2025-12-08 20:10:03 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
-// JavaScript Document
-console.log("hi");
+let menu = document.querySelector("#menu");
+let menuCloseButton = document.querySelector("#close-menu");
+let menuOpenButton = document.querySelector("#open-menu");
+
+function closeMenu() {
+  menu.style.left = "-9999em";
+}
+
+function openMenu() {
+  menu.style.left = "0";
+}
+
+menuCloseButton.addEventListener("click", closeMenu)
+menuOpenButton.addEventListener("click", openMenu)
+
